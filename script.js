25eebc906fdf4892c21bccffa0b623b2a058289e
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-10-30 11:59:58 -0300

    Entrando e mantendo a conexão
    O usuário é recebido com um prompt. Se o nome for existente pede para inserir outro. A conexão se mantém

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
+let mensagens =[];
+let nomeUsuario="";
+const rolarMensagensAutomaticamente = document.querySelector(".caixa-mensagem");
+rolarMensagensAutomaticamente.scrollIntoView();
 
-let mensagens =[]
-const rolarMensagensAutomaticamente = document.querySelector("main");
-rolarMensagensAutomaticamente.scrollIntoView()
 
-pegarDados();
+nomeUser ();
+// pegarDados();
+// entrarSala();
+
+
+
+function nomeUser(){
+    nomeUsuario = prompt ("Qual seu lindo nome, chuchu?");
+    entrarSala();
+    pegarDados();
+    
+}    
+
+function entrarSala(){
+const user = {
+    name: nomeUsuario
+}
+   const promessaUsuario= axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ", user);
+   promessaUsuario.then(entrouUsuario);
+   promessaUsuario.catch(erroUsuario)
+
+}
+
+function manterConexao(){
+    // console.log("Mantendo a conexão!")
+    const user = {
+        name: nomeUsuario
+    }
+       const promessaConexao= axios.post("https://mock-api.driven.com.br/api/v6/uol/status", user);
+//        promessaConexao.then(entrouConexao);
+//        promessaConexao.catch(erroConexao)
+ }
+
+function entrouUsuario(status){
+    console.log(status.status + "," + status.statusText);
+}
+
+function erroUsuario(statusErro){
+    alert("Esse nome já está logado em nosso sistema! Digite um outro nome ")
+    console.log(statusErro.response.status + "," + statusErro.response.statusText);
+nomeUser();
+}
+
+
 function pegarDados(){
+    // console.log("pegando os dados")
     const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promessa.then(renderizarMensagens);
     promessa.catch(dadosError);
 }
-
 
 
 function renderizarMensagens(dadosColetados){
@@ -41,10 +85,9 @@ for (let i=0; i<mensagens.length; i++){
         } 
 
 }
-setTimeout(pegarDados, 3000)
+setTimeout(pegarDados, 3000);
+setTimeout(manterConexao, 5000);
 }
-
-
 
 function dadosError(erro){
     console.log(erro);
