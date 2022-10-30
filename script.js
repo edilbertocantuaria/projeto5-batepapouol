e7ff4ebba0ada2378294a0b30bcb1909fc83210d
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-10-30 00:53:48 -0300

    Mensagens do servidor
    O programa pega as mensagens do servidor, mas apaga todas as outras mensagens

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+
+let mensagens =[]
+pegarDados();
+function pegarDados(){
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promessa.then(renderizarMensagens);
+    promessa.catch(dadosError);
+}
+
+
+
+function renderizarMensagens(dadosColetados){
+    mensagens = dadosColetados.data;
+    console.log(mensagens)
+
+    const chatInicial = document.querySelector("main")
+    console.log (typeof(chatInicial));
+    // chatInicial.innerHTML="";
+
+for (let i=0; i<mensagens.length; i++){
+    let mensagem_dados = mensagens[i]
+    if (mensagem_dados.text==="entra na sala..." ||mensagem_dados.text==="sai da sala..." ){
+        chatInicial.innerHTML=`
+        <div  class="caixa-mensagem entrou-saiu">
+        <div class="horario">(${mensagem_dados.time})</div>
+        <div class="mensagem"> <span>${mensagem_dados.from} </span>${mensagem_dados.text}</div>
+    </div>`
+        } else if(mensagem_dados.to==="Todos"){
+            chatInicial.innerHTML=`
+            <div  class="caixa-mensagem mensagemPublica">
+            <div class="horario">(${mensagem_dados.time})</div>
+            <div class="mensagem"> <span>${mensagem_dados.from} </span>para <span>${mensagem_dados.to}:</span> ${mensagem_dados.text}</div>
+        </div>`
+        } else if  (mensagem_dados.to!="Todos"){
+            chatInicial.innerHTML=`
+            <div  class="caixa-mensagem mensagemReservada">
+            <div class="horario">(${mensagem_dados.time})</div>
+            <div class="mensagem"> <span>${mensagem_dados.from} </span>reservadamente para <span>${mensagem_dados.to}:</span> ${mensagem_dados.text}</div>
+            </div>`
+        } 
+    
+
+}
+}
+
+
+
+function dadosError(erro){
+    console.log(erro);
+}
+
 function enviarMensagem(){
     console.log ("Funcionando o botão de enviar mensagem");
 }
